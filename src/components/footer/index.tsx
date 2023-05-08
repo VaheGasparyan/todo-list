4d284e1408bd14010843a99ff906c60d05ac5064
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-08 18:24:43 +0400

    create switch theme button

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,14 +1,20 @@
 import './footer.css';
+import {MaterialUISwitch} from "../switch";
 
 const Footer = () => {
     return (
         <footer>
-            <div className="all">
-                <p>all tasks 0</p>
+            <div className="wrapper">
+                <div className="all">
+                    <p>all tasks 0</p>
+                </div>
+                <span className='slash'>/</span>
+                <div className="completed">
+                    <p>completed tasks 0</p>
+                </div>
             </div>
-            <span>/</span>
-            <div className="completed">
-                <p>completed tasks 0</p>
+            <div className="switch">
+                <MaterialUISwitch />
             </div>
         </footer>
     );
--- src/todo/index.tsx
+++ src/todo/index.tsx
@@ -2,6 +2,7 @@ import TodoForm from "components/todo_form";
 import Footer from "components/footer";
 
 import './todo.css';
+import {MaterialUISwitch} from "../components/switch";
 
 const Todo = () => {
     return (
