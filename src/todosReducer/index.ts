428e510b78e8b3fe732a275e58a9b1df4dae662c
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-09 15:58:37 +0400

    create edit and close functional

---
+++ src/todosReducer/index.ts
@@ -0,0 +1,32 @@
+import {ITodo} from "interfaces/todoState";
+import {actionsTypes} from "actions";
+import {filterState} from "utils/filterState";
+import { changeIsEdit } from 'utils/changeIsEdit';
+import { changeText } from 'utils/changeText';
+
+export const todosReducer = (prevState: ITodo[], action: any) => {
+    switch (action.type) {
+        case actionsTypes.add:
+            return [{...action.payload}, ...prevState];
+        case actionsTypes.delete:
+            return filterState(prevState, action.payload.id);
+        case actionsTypes.edit:
+            if(!action.payload.inputValue) {
+                return changeIsEdit(prevState, action.payload.id);
+            } else {
+                return changeText(prevState, action.payload.id, action.payload.inputValue);
+            }
+        case actionsTypes.close:
+            return prevState.map(todo => {
+                if(todo.id === action.payload.id) {
+                    return {
+                        ...todo,
+                        isEdit: false
+                    }
+                }
+
+                return todo
+            })
+        default: return prevState;
+    }
+}
