7c5785315686650bafc3b45a2121144146454868
VaheGasparyan <vahegsparyan@gmail.com>
2023-05-10 15:03:09 +0400

    create app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Todo from "todo";
 
 import './App.css';
+import {useState} from "react";
 
 const App = () => {
+    const [theme, setTheme] = useState('dark');
+
   return (
     <div className="App">
       <Todo />
